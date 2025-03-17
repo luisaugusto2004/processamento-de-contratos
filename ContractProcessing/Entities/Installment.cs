3a345de7dd75cb25cc3ec8b9927650ff63e9daff
Luis Augusto <luisaugusto010203@gmail.com>
2025-03-17 01:06:19 -0300

    primeiro commit

---
+++ ContractProcessing/Entities/Installment.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace ContractProcessing.Entities
+{
+    class Installment
+    {
+        public DateTime dueDate { get; set; }
+        public double amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            this.dueDate = dueDate;
+            this.amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return dueDate.ToString("dd/MM/yyy") + " - R$" + amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContractProcessing/Services/IPaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace ContractProcessing.Services
+{
+    interface IPaymentService
+    {
+        public double Interest(double amount, int months);
+        public double Fees(double amount);
+    }
+}
